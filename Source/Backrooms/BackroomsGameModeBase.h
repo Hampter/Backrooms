a340396aa5fea1831839d173978d5097a4406fb0
Noah Springer <noah.springer1113@gmail.com>
2022-05-28 16:43:55 -0400

    Initial commit

---
+++ Source/Backrooms/BackroomsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BackroomsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BACKROOMS_API ABackroomsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
